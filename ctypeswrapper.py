b5291ae2c5a220a91f23f9ed67502c53d5805f40
Christoph Haag <haagch.christoph@googlemail.com>
2013-04-02 16:04:06 +0200

    don't import * from ctypes

--- ctypeswrapper.py
+++ ctypeswrapper.py
@@ -2,7 +2,7 @@
 libname = "xinput2wrapper.so.1"
 
 import os
-from ctypes import *
+import ctypes
 import decimal
 
 # From X11/extensions/XI2.h
@@ -20,10 +20,10 @@ usestrings = {1: "XIMasterPointer", 2: "XIMasterKeyboard", 3: "XISlavePointer", 
 #    int         type;
 #    int         sourceid;
 #} XIAnyClassInfo;
-class XIAnyClassInfo_struct(Structure):
+class XIAnyClassInfo_struct(ctypes.Structure):
         _fields_ = [
-                ("type", c_int),
-                ("sourceid", c_int)
+                ("type", ctypes.c_int),
+                ("sourceid", ctypes.c_int)
         ]
 
 #typedef struct
@@ -36,20 +36,20 @@ class XIAnyClassInfo_struct(Structure):
 #    int                 num_classes;
 #    XIAnyClassInfo      **classes;
 #} XIDeviceInfo;
-class XIDeviceInfo_struct(Structure):
+class XIDeviceInfo_struct(ctypes.Structure):
     _fields_ = [
-            ("deviceid", c_int),
-            ("name", c_char_p),
-            ("use", c_int),
-            ("attachment", c_int),
-            ("enabled", c_int),
-            ("classes", POINTER(XIAnyClassInfo_struct))
+            ("deviceid", ctypes.c_int),
+            ("name", ctypes.c_char_p),
+            ("use", ctypes.c_int),
+            ("attachment", ctypes.c_int),
+            ("enabled", ctypes.c_int),
+            ("classes", ctypes.POINTER(XIAnyClassInfo_struct))
             ]
 
-lib = cdll.LoadLibrary(libname)
-lib.e_version.restype = c_char_p
+lib = ctypes.cdll.LoadLibrary(libname)
+lib.e_version.restype = ctypes.c_char_p
 xinput_version = lib.e_version().decode("utf8")
-lib.e_numdevices.restype = c_int
+lib.e_numdevices.restype = ctypes.c_int
 lib.e_list.restype = XIDeviceInfo_struct
 
 if decimal.Decimal(xinput_version) >= decimal.Decimal(2):
@@ -61,7 +61,7 @@ def XIDeviceInfoList():
   numdevices = lib.e_numdevices()
   l = dict()
   for i in range(numdevices):
-    info = lib.e_list(c_int(i))
+    info = lib.e_list(ctypes.c_int(i))
     l[info.deviceid] = {
     'deviceid': info.deviceid,
     'name': info.name.decode("utf8"),
@@ -76,7 +76,7 @@ def XIDeviceInfoList():
 if __name__ == "__main__":
   numdevices = lib.e_numdevices()
   for i in range(numdevices):
-      info = lib.e_list(c_int(i))
+      info = lib.e_list(ctypes.c_int(i))
       print ("Device id: " + str(info.deviceid))
       print ("Device name: " + str(info.name.decode("utf8")))
       print ("Device use: " + str(info.use) + " -> " + usestrings[info.use])
